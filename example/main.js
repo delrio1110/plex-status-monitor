a665418781bb3c2729a47a30a940b1d39ab36e33
Kevin Knopp <kevin.knopp@rosetta.com>
2016-04-01 01:54:00 -0700

    updates

--- example/main.js
+++ example/main.js
@@ -1,5 +1,11 @@
 var menubar = require('menubar')
-
+require('electron-debug')({
+    showDevTools: true
+});
+var handlebars = require('handlebars');
+// var jsdom = require("jsdom");
+// var $ = require("jquery")
+// var cheerio = require('cheerio');
 var mb = menubar()
 
 mb.on('ready', function ready () {
